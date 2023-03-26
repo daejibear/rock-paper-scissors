bea8eebc097a36ce89e0c6d585887da338a9997d
daejibear <118245676+daejibear@users.noreply.github.com>
2023-03-27 08:23:36 +0900

    commit test

---
+++ script.js
@@ -0,0 +1,33 @@
+let playerSelection = null;
+let computerSelection = null;
+
+
+
+
+function getComputerChoice () {
+
+}
+
+
+function game() {
+
+
+}
+
+
+/* pseudocode process:
+game rule:
+
+rock paper scissors
+
+player selection = 3 choices
+computer selection = 3 choices --- determined by a random number generation 0-2
+0 = rock
+1 = paper
+2 = scissors
+
+game process:
+
+player selects option --> computer choice --> output
+
+*/
