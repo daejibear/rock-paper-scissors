59bb1bc058850f75d3b2b807e083904f87cc3a92
daejibear <118245676+daejibear@users.noreply.github.com>
2023-03-27 19:25:05 +0900

    added score - 3 score win

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ let computerSelection = ranNum ();
 let btn = document.querySelectorAll('button');
 let playerEl = document.getElementById('player');
 let computerEl = document.getElementById('computer');
-let outcomeEl = document.getElementById('outcome')
-let playerHandImg = document.getElementById("player-hand")
-let computerHandImg = document.getElementById("computer-hand")
+let outcomeEl = document.getElementById('outcome');
+let playerHandImg = document.getElementById("player-hand");
+let computerHandImg = document.getElementById("computer-hand");
+let playerScoreEl =  document.getElementById('player-score');
+let computerScoreEl = document.getElementById('computer-score');
+let playerScore = 0;
+let computerScore = 0;
+
+//player Score Count
+
+
 //button selection
 btn.forEach((button) => { 
     button.addEventListener ('click', (event) => {
@@ -28,21 +36,38 @@ randomNumber =
 //game rules:
 function gameOutcome () {
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        outcomeEl.textContent = "player wins!"
+        outcomeEl.textContent = "player wins!";
+     playerScore += 1;
         } else if (playerSelection == "paper" && computerSelection == "rock") {
-            outcomeEl.textContent = "player wins!"
+            outcomeEl.textContent = "player wins!";
+            playerScore += 1;
         } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            outcomeEl.textContent = "player wins!"
+            outcomeEl.textContent = "player wins!";
+            playerScore += 1;
         } else if ( playerSelection == "scissors" && computerSelection == "scissors") {
-            outcomeEl.textContent = "it's a draw!"
+            outcomeEl.textContent = "it's a draw!";
         } else if ( playerSelection == "paper" && computerSelection == "paper") {
-            outcomeEl.textContent = "it's a draw!"
+            outcomeEl.textContent = "it's a draw!";
         } else if ( playerSelection == "rock" && computerSelection == "rock") {
-            outcomeEl.textContent = "it's a draw!"
+            outcomeEl.textContent = "it's a draw!";
         } else {
             outcomeEl.textContent = "computer wins!";
+          computerScore += 1;
         }
     };
+
+
+//winner wins after 3 wins
+function score () {
+    if (playerScore == 3) {
+        alert("You win the game!")
+        resetScore();
+     }else if (computerScore == 3) {
+        alert("You lost the game!")
+        resetScore();
+     }
+        console.log(`after${playerScore}`)
+    }
 
 //game results:
 function game () {
@@ -51,8 +76,28 @@ function game () {
     computerHandImg.src=`images/p-${computerSelection}.png`;
     playerEl.textContent = `player:${playerSelection}`;
     computerEl.textContent = `computer: ${computerSelection}`;
+    console.log(`before${playerScore}`)
     gameOutcome();
+    playerScoreEl.textContent = playerScore;
+computerScoreEl.textContent = computerScore;
+
+
+setTimeout("score()",500);
+
+
+
 }
+
+    //reset scores
+    function resetScore () {
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreEl.textContent = playerScore;
+        computerScoreEl.textContent = computerScore;
+        }
+    
+
+
 /* pseudocode process:
 game rule:
 
